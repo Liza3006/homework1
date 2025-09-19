83a85cdc0d35f3d89491ed9780259833d7d50a10
elizaveta_orlova <eko0720@mail.ru>
2025-09-20 02:47:06 +0300

    Added classes. Third task

---
+++ src/main/java/com/mipt/orlovaelizaveta/WorkingPeople.java
@@ -0,0 +1,8 @@
+package com.mipt.orlovaelizaveta;
+
+public abstract class WorkingPeople {
+    public abstract void work(int anyInt);
+    public boolean goHome(String fString, String sString) {
+        return fString.equals(sString);
+    }
+}
