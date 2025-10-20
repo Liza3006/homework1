bf7a45e6be27923c6dccdf00a56d9749fa4208c2
elizaveta_orlova <eko0720@mail.ru>
2025-10-20 19:26:00 +0300

    Task_7

--- src/test/java/hw_7/CustomArrayListTest.java
+++ src/test/java/hw_7/CustomArrayListTest.java
@@ -3,7 +3,6 @@ package hw_7;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Iterator;
 
 class CustomArrayListTest {
