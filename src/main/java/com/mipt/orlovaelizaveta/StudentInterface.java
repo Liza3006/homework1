89a9c0cf655fc223ef16fa60a7efe61179904d31
Орлова Елизавета Константиновна <eliseagle07@gmail.com>
2025-10-20 15:36:55 +0300

    Delete src/main/java/com/mipt/orlovaelizaveta directory

--- src/main/java/com/mipt/orlovaelizaveta/StudentInterface.java
+++
@@ -1,5 +0,0 @@
-package com.mipt.orlovaelizaveta;
-
-public interface StudentInterface {
-    Object study(Object anyObject);
-}
--- src/main/java/com/mipt/orlovaelizaveta/WorkingPeople.java
+++
@@ -1,8 +0,0 @@
-package com.mipt.orlovaelizaveta;
-
-public abstract class WorkingPeople {
-    public abstract void work(int anyInt);
-    public boolean goHome(String fString, String sString) {
-        return fString.equals(sString);
-    }
-}
